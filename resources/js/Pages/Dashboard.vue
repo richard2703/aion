520be8406991bcfc09c282180a11d79007743259
frontmappriskdr <leonardoperezf23@gmail.com>
2024-11-24 13:19:25 -0400

    create component tab

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,8 +4,14 @@
 
 <template>
     <Layout2>
-        <div class="p-10">
-            <h1>Here is a child componendt!</h1>
+        <div>
+            <div class="border-b-[1px] border-gray-200 p-7 pl-10">
+
+               <div class="border-[1px] border-gray-200 p-4"> 
+                <p>Gente y cultura</p>
+               </div>
+
+            </div>
         </div>
     </Layout2>
 </template>
