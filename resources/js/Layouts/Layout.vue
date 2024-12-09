bc4d49f46a13bc0fb23991190d3e7abd366d2c1b
jesuslim <limchuy7@gmail.com>
2024-12-09 13:29:52 -0600

    feat: filtrado de elementos por roles designados

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -8,8 +8,10 @@
                 <button @click="isOpen = !isOpen" class="text-white md:hidden">
                     <i class="pi pi-bars text-2xl"></i>
                 </button>
-                <Logo class="h-10" />
-                <span class="text-lg font-bold">AION Business</span>
+                <Link href="/" class="flex items-center gap-2">
+                    <Logo class="h-10" />
+                    <span class="text-lg font-bold">AION Business</span>
+                </Link>
             </div>
             <nav class="flex items-center gap-4">
                 <button class="relative text-gray-300 hover:text-white">
@@ -72,21 +74,22 @@
         </header>
 
         <!-- Content Area -->
-        <div class="flex flex-1 h-full">
+        <div class="flex flex-1 h-full mb-4">
             <!-- Sidebar -->
             <div
                 :class="[
-                    'bg-gray-800 text-white w-64 p-4 space-y-4',
+                    'bg-gray-800 text-white w-64 p-4',
                     { hidden: !isOpen, block: isOpen },
                     'md:block',
                     'z-50',
                     'md:static',
                     'absolute',
-                    'h-screen',
+                    'h-screen overflow-auto',
+                    'mb-4',
                 ]"
             >
                 <nav class="flex flex-col space-y-2">
-                    <div v-for="item in menuItems" :key="item.label">
+                    <div v-for="item in filteredMenu" :key="item.label">
                         <button
                             @click="
                                 item.children
@@ -119,7 +122,7 @@
                         </button>
                         <div
                             v-if="item.children && openMenus.includes(item)"
-                            class="pl-6 space-y-2 overflow-y-auto max-h-screen"
+                            class="pl-6 space-y-2 max-h-screen"
                         >
                             <button
                                 v-for="child in item.children"
@@ -156,184 +159,220 @@
 <script setup>
 import { ref } from "vue";
 import Logo from "./Shared/Logo.vue";
+import { Link, usePage } from "@inertiajs/vue3";
 
 const isOpen = ref(false);
 const profileMenuOpen = ref(false);
 const openMenus = ref([]);
+const userAuth = usePage().props.auth.user.roles;
+console.log({ userAuth });
 
 const menuItems = [
-    { label: "Alineacion", route: "dashboard", icon: "" },
+    {
+        label: "Alineacion",
+        route: "dashboard",
+        icon: "",
+        role: ["admin", "user", "superadmin", "lider_pilar"],
+    },
     {
         label: "Identificacion",
         icon: "",
+        role: ["admin", "superadmin", "user"],
         children: [
             {
                 label: "Evaluaciones",
                 route: "evaluacion.index",
                 icon: "",
+                role: ["lider_pilar", "admin", "superadmin", "user"],
             },
             {
                 label: "Metas (30 60 90)",
                 route: "metas.index",
                 icon: "",
+                role: ["admin", "superadmin", "user"],
             },
             {
                 label: "Mis Metas (30 60 90)",
                 route: "Mismetas.index",
                 icon: "",
+                role: ["admin", "superadmin", "user"],
             },
             {
                 label: "Mis Reportes",
                 route: "misreporte.index",
                 icon: "",
+                role: ["admin", "superadmin", "user"],
             },
             {
                 label: "Desperdicios",
                 route: "desperdicio.index",
                 icon: "",
+                role: ["admin", "superadmin", "user"],
             },
         ],
     },
     {
         label: "Optimizacion",
         icon: "",
+        role: ["admin", "superadmin", "user"],
         children: [
             {
                 label: "Dashboard",
                 route: "sysgestion.dashboard",
                 icon: "",
+                role: ["admin", "superadmin", "user"],
             },
             {
                 label: "PDCA",
                 route: "sysgestion.index",
                 icon: "",
+                role: ["admin", "superadmin", "user"],
             },
             {
                 label: "Minutas",
                 route: "minutas.index",
                 icon: "",
+                role: ["admin", "superadmin", "user"],
             },
             {
                 label: "Reportes",
                 route: "reporte.index",
                 icon: "",
+                role: ["admin", "superadmin", "user"],
             },
             {
                 label: "Mis Reportes",
                 route: "misreporte.index",
                 icon: "",
+                role: ["admin", "superadmin", "user"],
             },
             {
                 label: "ScoreCard",
                 route: "scoreCard.index",
                 icon: "",
+                role: ["admin", "superadmin", "user"],
             },
         ],
     },
     {
         label: "Networking",
         icon: "",
+        role: ["admin", "user", "superadmin"],
         children: [
             {
                 label: "Capacitacion",
                 route: "https://laproductivitycenter.com",
                 icon: "",
+                role: ["admin", "user", "superadmin", "lider_pilar"],
             },
         ],
     },
     {
         label: "Configuraciones",
         icon: "pi pi-cog",
+        role: ["admin", "superadmin"],
         children: [
             {
                 label: "Pilares",
                 route: "area.index",
                 icon: "",
+                role: ["admin", "superadmin"],
             },
             {
                 label: "Flujos de valor",
                 route: "departamento.index",
                 icon: "",
+                role: ["admin", "superadmin"],
             },
             {
                 label: "Usuarios",
                 route: "user.index",
                 icon: "",
+                role: ["admin", "superadmin"],
             },
             {
                 label: "Líderes de flujo",
                 route: "encargadoFlujo.index",
                 icon: "",
+                role: ["admin", "superadmin"],
             },
             {
                 label: "Tipos de Desperdicios",
                 route: "tiposDesperdicios.index",
                 icon: "",
+                role: ["admin", "superadmin"],
             },
             {
                 label: "Eventos",
                 route: "eventos.index",
                 icon: "",
+                role: ["superadmin"], // TODO: LIMITAR A SUPERADMIN Y LIDER_PILAR ( PRINCIPALMENTE A GYC)
             },
             {
                 label: "Secciones",
                 route: "seccion.index",
                 icon: "",
+                role: ["superadmin"],
             },
             {
                 label: "Challenges",
                 route: "challenge.index",
                 icon: "",
+                role: ["superadmin"],
             },
             {
                 label: "Opciones",
                 route: "opcion.index",
                 icon: "",
+                role: ["superadmin"],
             },
             {
                 label: "Configuración de Alineación",
                 route: "config.index",
                 icon: "",
+                role: ["admin", "superadmin"],
             },
             {
                 label: "Objetivo",
                 route: "objetivo.index",
                 icon: "",
+                role: ["admin", "superadmin"],
             },
             {
                 label: "Procesos",
                 route: "proceso.index",
                 icon: "",
+                role: ["admin", "superadmin"],
             },
             {
                 label: "Procedimientos",
                 route: "procedimiento.index",
                 icon: "",
+                role: ["admin", "superadmin"],
             },
             {
                 label: "Estandares",
                 route: "estandar.index",
                 icon: "",
+                role: ["admin", "superadmin"],
             },
             {
                 label: "KPI's",
                 route: "kpi.index",
                 icon: "",
+                role: ["admin", "superadmin"],
             },
             {
                 label: "Roles",
                 route: "roles.index",
                 icon: "",
+                role: ["admin", "superadmin"],
             },
             {
                 label: "Permisos",
                 route: "permisos.index",
                 icon: "",
-            },
-            {
-                label: "",
-                route: "",
-                icon: "",
+                role: ["admin", "superadmin"],
             },
         ],
     },
@@ -360,6 +399,24 @@ function logout() {
     console.log("Logging out");
     // Implement logout logic
 }
+
+// const hasRole = (role) => roles.includes(role);
+// Función para filtrar el menú según roles
+const filterMenuByRole = (menu, roles) => {
+    return menu
+        .filter((item) => item.role.some((role) => roles.includes(role))) // Filtra elementos con roles válidos
+        .map((item) => ({
+            ...item,
+            children: item.children
+                ? filterMenuByRole(item.children, roles)
+                : undefined, // Filtra hijos recursivamente
+        }))
+        .filter((item) => !item.children || item.children.length > 0); // Remueve padres sin hijos válidos
+};
+
+// Menú filtrado
+const filteredMenu = filterMenuByRole(menuItems, userAuth);
+console.log({ filteredMenu });
 </script>
 
 <style>
