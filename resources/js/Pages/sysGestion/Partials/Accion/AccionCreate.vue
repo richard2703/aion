d2af6795ed4b00d0027720d763a9887674bb485f
frontmappriskdr <leonardoperezf23@gmail.com>
2024-12-19 18:16:10 -0400

    item nuevo sistema de gestion

--- resources/js/Pages/sysGestion/Partials/Accion/AccionCreate.vue
+++ resources/js/Pages/sysGestion/Partials/Accion/AccionCreate.vue
@@ -108,17 +108,17 @@ const submit = async () => {
 <template>
     <div class="w-full">
         <h2 class="text-center text-xl">Nueva acción</h2>
-        <form @submit.prevent="submit" class="w-full ">
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
+        <form @submit.prevent="submit" class="w-full">
+            <div class="gap-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2">
                 <div class="mt-4">
                     <InputLabel for="nombre" value="Nombre:" />
-                    <TextInput id="nombre" v-model="form.titulo" type="text" class="mt-1 block w-full" required
+                    <TextInput id="nombre" v-model="form.titulo" type="text" class="block mt-1 w-full" required
                         autocomplete="nombre" />
                 </div>
                 <div class="mt-4">
                     <InputLabel for="proceso" value="Proceso:" />
                     <select ref="proceso_select"
-                        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full px-3 py-2 cursor-pointer"
+                        class="border-gray-300 focus:border-indigo-500 shadow-sm px-3 py-2 rounded-md focus:ring-indigo-500 w-full cursor-pointer"
                         v-model="form.proceso_id" required>
                         <option value="" disabled selected>
                             Seleccione una opcion
@@ -130,7 +130,7 @@ const submit = async () => {
                 </div>
             </div>
 
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
+            <div class="gap-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2">
                 <div class="mt-4">
                     <InputLabel for="responsable_id" value="Responsable:" />
                     <AutoComplete v-model="form.responsable_id" optionLabel="name" :suggestions="filteredUsuarios"
@@ -139,7 +139,7 @@ const submit = async () => {
                 <div class="mt-4">
                     <InputLabel for="estatus" value="Estatus: " />
                     <select ref="departamento_select"
-                        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full px-3 py-2 cursor-pointer"
+                        class="border-gray-300 focus:border-indigo-500 shadow-sm px-3 py-2 rounded-md focus:ring-indigo-500 w-full cursor-pointer"
                         v-model="form.estatus_id" required>
                         <option value="" selected disabled>
                             Seleccione una opcion </option>
@@ -155,15 +155,15 @@ const submit = async () => {
                 </div>
             </div>
 
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
+            <div class="gap-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2">
                 <div class="mt-4">
                     <InputLabel for="link" value="Link:" />
-                    <TextInput id="link" v-model="form.link" type="text" class="mt-1 block w-full" required
+                    <TextInput id="link" v-model="form.link" type="text" class="block mt-1 w-full" required
                         autocomplete="link" />
                 </div>
             </div>
             <div class="m-4 py-2">
-                <PrimaryButton class="ms-4 pi pi-save float-right" :class="{ 'opacity-25': form.processing }"
+                <PrimaryButton class="float-right ms-4 pi pi-save" :class="{ 'opacity-25': form.processing }"
                     :disabled="form.processing">
 
                 </PrimaryButton>
