a8e314954994ba372a823f1ef5b7fabc3e87ab1f
jesuslim <limchuy7@gmail.com>
2025-01-08 12:08:57 -0600

    feat: limitar botones de minutas

--- resources/js/Pages/Minutas/Partials/TabPanel/ContenidoPanel.vue
+++ resources/js/Pages/Minutas/Partials/TabPanel/ContenidoPanel.vue
@@ -8,7 +8,8 @@ import TextInput from "@/Components/TextInput.vue";
 import { format } from "date-fns";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import { confirmDialog, showToast } from "@/Pages/utils/SweetAlert.service";
-import { useForm } from "@inertiajs/vue3";
+import { useForm, usePage } from "@inertiajs/vue3";
+
 const props = defineProps({
     area: Object,
     departamento_id: Number,
@@ -38,6 +39,7 @@ const desde = ref("");
 const hasta = ref("");
 const tier = ref("");
 const tiposMinutas = ref([]);
+const userPermissions = usePage().props.auth.user.permissions;
 
 const formFilter = useForm({
     area_id: "",
@@ -243,34 +245,55 @@ const getTiposMinuta = async () => {
 };
 
 const flujoBody = (rowData) => {
-    return rowData.departamento?.nombre || 'Todos los flujos';
+    return rowData.departamento?.nombre || "Todos los flujos";
 };
 </script>
 
 <template>
     <div class="overflow-x-auto mt-4">
         <div class="flex gap-4">
-            <InputText v-model="globalFilter" placeholder="Buscar..." class="mb-3" />
-            <PrimaryButton class="mb-4 float-right pi pi-filter" @click="openFilter">
+            <InputText
+                v-model="globalFilter"
+                placeholder="Buscar..."
+                class="mb-3"
+            />
+            <PrimaryButton
+                class="mb-4 float-right pi pi-filter"
+                @click="openFilter"
+            >
             </PrimaryButton>
-            <PrimaryButton v-if="customFilter" class="mb-4 float-right pi pi-times" @click="clearFilter">
+            <PrimaryButton
+                v-if="customFilter"
+                class="mb-4 float-right pi pi-times"
+                @click="clearFilter"
+            >
             </PrimaryButton>
         </div>
 
         <!-- formulario de filtrado de tareas -->
         <div v-if="customFilter" class="">
             <form @submit.prevent="filterTable()">
-                <div class="m-4 border rounded-lg border-gray-200 flex gap-2 grid grid-cols-4">
+                <div
+                    class="m-4 border rounded-lg border-gray-200 flex gap-2 grid grid-cols-4"
+                >
                     <div class="m-4">
-                        <InputLabel for="departamento_id" value="Flujo de valor: " />
-                        <select ref="departamento_select"
+                        <InputLabel
+                            for="departamento_id"
+                            value="Flujo de valor: "
+                        />
+                        <select
+                            ref="departamento_select"
                             class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full px-3 py-2 cursor-pointer"
-                            v-model="flujoValor">
+                            v-model="flujoValor"
+                        >
                             <option value="" selected>
                                 Seleccione una opcion
                             </option>
-                            <option v-for="departamento in departamentos" :key="departamento.id"
-                                :value="departamento.id">
+                            <option
+                                v-for="departamento in departamentos"
+                                :key="departamento.id"
+                                :value="departamento.id"
+                            >
                                 {{ departamento.nombre }}
                             </option>
                         </select>
@@ -278,13 +301,19 @@ const flujoBody = (rowData) => {
 
                     <div class="m-4">
                         <InputLabel for="lider_id" value="Lider: " />
-                        <select ref="lider_select"
+                        <select
+                            ref="lider_select"
                             class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full px-3 py-2 cursor-pointer"
-                            v-model="lider">
+                            v-model="lider"
+                        >
                             <option value="" selected>
                                 Seleccione una opcion
                             </option>
-                            <option v-for="usuario in usuarios" :key="usuario.id" :value="usuario.id">
+                            <option
+                                v-for="usuario in usuarios"
+                                :key="usuario.id"
+                                :value="usuario.id"
+                            >
                                 {{ usuario.name }}
                             </option>
                         </select>
@@ -293,13 +322,20 @@ const flujoBody = (rowData) => {
                     <div class="mt-4">
                         <InputLabel for="tipo" value="Tipo: " />
 
-                        <select ref="tipo_select"
+                        <select
+                            ref="tipo_select"
                             class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full px-3 py-2 cursor-pointer"
-                            v-model="tipo" required>
+                            v-model="tipo"
+                            required
+                        >
                             <option value="" disabled selected>
                                 Seleccione una opcion
                             </option>
-                            <option v-for="tipoMinuta in tiposMinutas" :key="tipoMinuta.id" :value="tipoMinuta.id">
+                            <option
+                                v-for="tipoMinuta in tiposMinutas"
+                                :key="tipoMinuta.id"
+                                :value="tipoMinuta.id"
+                            >
                                 {{ tipoMinuta.titulo }}
                             </option>
                         </select>
@@ -307,13 +343,19 @@ const flujoBody = (rowData) => {
 
                     <div class="m-4">
                         <InputLabel for="proceso_id" value="Proceso: " />
-                        <select ref="departamento_select"
+                        <select
+                            ref="departamento_select"
                             class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full px-3 py-2 cursor-pointer"
-                            v-model="proceso">
+                            v-model="proceso"
+                        >
                             <option value="" selected>
                                 Seleccione una opcion
                             </option>
-                            <option v-for="proceso in procesos" :key="proceso.id" :value="proceso.id">
+                            <option
+                                v-for="proceso in procesos"
+                                :key="proceso.id"
+                                :value="proceso.id"
+                            >
                                 {{ proceso.nombre }}
                             </option>
                         </select>
@@ -321,31 +363,49 @@ const flujoBody = (rowData) => {
 
                     <div class="m-4">
                         <InputLabel for="fecha" value="Fecha de entrega de: " />
-                        <TextInput id="fecha" v-model="desde" type="date" class="mt-1 block w-full"
-                            autocomplete="fecha" />
+                        <TextInput
+                            id="fecha"
+                            v-model="desde"
+                            type="date"
+                            class="mt-1 block w-full"
+                            autocomplete="fecha"
+                        />
                     </div>
 
                     <div class="m-4">
-                        <InputLabel for="created_at" value="Fecha de entrega hasta: " />
-                        <TextInput id="fecha" v-model="hasta" type="date" class="mt-1 block w-full"
-                            autocomplete="fecha" />
+                        <InputLabel
+                            for="created_at"
+                            value="Fecha de entrega hasta: "
+                        />
+                        <TextInput
+                            id="fecha"
+                            v-model="hasta"
+                            type="date"
+                            class="mt-1 block w-full"
+                            autocomplete="fecha"
+                        />
                     </div>
                     <div class="m-4">
-                        <InputLabel for="created_at" value="Nivel de minuta: " />
-                        <select ref="departamento_select"
+                        <InputLabel
+                            for="created_at"
+                            value="Nivel de minuta: "
+                        />
+                        <select
+                            ref="departamento_select"
                             class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full px-3 py-2 cursor-pointer"
-                            v-model="tier">
+                            v-model="tier"
+                        >
                             <option value="" selected>
                                 Seleccione una opcion
                             </option>
-                            <option value=3>
-                                3 Líderes flujo de valor - Equipo </option>
-                            <option value=2>
-                                2 Gerentes de pilar - líderes flujo de valor</option>
-                            <option value=1>
-                                1 staff directivo</option>
-                            <option value=0>
-                                0 Consejo de Admin</option>
+                            <option value="3">
+                                3 Líderes flujo de valor - Equipo
+                            </option>
+                            <option value="2">
+                                2 Gerentes de pilar - líderes flujo de valor
+                            </option>
+                            <option value="1">1 staff directivo</option>
+                            <option value="0">0 Consejo de Admin</option>
                             <!-- <option value="3" selected>3</option>
                             <option value="2" selected>2</option>
                             <option value="1" selected>1</option>
@@ -356,9 +416,19 @@ const flujoBody = (rowData) => {
             </form>
         </div>
 
-        <DataTable :value="minutas" paginator :rows="rows" :totalRecords="totalRecords" :lazy="true" :first="first"
-            @page="onPage" @sort="onSort" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="min-width: 50rem"
-            :filters="filters" :globalFilterFields="[
+        <DataTable
+            :value="minutas"
+            paginator
+            :rows="rows"
+            :totalRecords="totalRecords"
+            :lazy="true"
+            :first="first"
+            @page="onPage"
+            @sort="onSort"
+            :rowsPerPageOptions="[5, 10, 20, 50]"
+            tableStyle="min-width: 50rem"
+            :filters="filters"
+            :globalFilterFields="[
                 'id',
                 'departamento.nombre',
                 'alias',
@@ -366,38 +436,92 @@ const flujoBody = (rowData) => {
                 'lider.name',
                 'notas',
                 'created_at',
-            ]" :sortField="sortField" :sortOrder="sortOrder"
-            class="p-datatable-sm p-datatable-striped p-datatable-gridlines">
+            ]"
+            :sortField="sortField"
+            :sortOrder="sortOrder"
+            class="p-datatable-sm p-datatable-striped p-datatable-gridlines"
+        >
             <template #empty> Sin registros </template>
-            <Column field="id" header="ID" headerStyle="width:4em;" bodyStyle="text-align:center;" sortable></Column>
+            <Column
+                field="id"
+                header="ID"
+                headerStyle="width:4em;"
+                bodyStyle="text-align:center;"
+                sortable
+            ></Column>
             <!-- <Column field="departamento?.nombre" header="Fujo de valor" headerStyle="width:4em;"
                 bodyStyle="text-align:center;" bodyClass="text-center" :body="flujoBody" sortable></Column> -->
-            <Column field="departamento?.nombre" header="Flujo de valor" headerStyle="width:4em;"
-                bodyStyle="text-align:center;" bodyClass="text-center" sortable>
+            <Column
+                field="departamento?.nombre"
+                header="Flujo de valor"
+                headerStyle="width:4em;"
+                bodyStyle="text-align:center;"
+                bodyClass="text-center"
+                sortable
+            >
                 <template #body="slotProps">
-                    {{ slotProps.data.departamento?.nombre || 'Todos los flujos' }}
+                    {{
+                        slotProps.data.departamento?.nombre ||
+                        "Todos los flujos"
+                    }}
                 </template>
             </Column>
-            <Column field="tipo_minuta.titulo" header="Tipo" headerStyle="width:4em;" bodyStyle="text-align:center;"
-                bodyClass="text-center" sortable></Column>
-            <Column field="alias" header="Alias" headerStyle="width:4em;" bodyClass="text-center" sortable></Column>
-            <Column field="proceso.nombre" header="Proceso" headerStyle="width:4em;" bodyClass="text-center" sortable>
+            <Column
+                field="tipo_minuta.titulo"
+                header="Tipo"
+                headerStyle="width:4em;"
+                bodyStyle="text-align:center;"
+                bodyClass="text-center"
+                sortable
+            ></Column>
+            <Column
+                field="alias"
+                header="Alias"
+                headerStyle="width:4em;"
+                bodyClass="text-center"
+                sortable
+            ></Column>
+            <Column
+                field="proceso.nombre"
+                header="Proceso"
+                headerStyle="width:4em;"
+                bodyClass="text-center"
+                sortable
+            >
             </Column>
-            <Column field="lider.name" header="Lider" headerStyle="width:4em;" bodyStyle="text-align:center;"
-                bodyClass="text-center" sortable></Column>
-            <Column header="fecha" headerStyle="width:4em;" bodyStyle="text-align:center;" bodyClass="text-center"
-                sortable>
+            <Column
+                field="lider.name"
+                header="Lider"
+                headerStyle="width:4em;"
+                bodyStyle="text-align:center;"
+                bodyClass="text-center"
+                sortable
+            ></Column>
+            <Column
+                header="fecha"
+                headerStyle="width:4em;"
+                bodyStyle="text-align:center;"
+                bodyClass="text-center"
+                sortable
+            >
                 <template #body="slotProps">
                     {{ formatearFecha(slotProps.data.created_at) }}
                 </template>
             </Column>
-            <Column header="Tareas" headerStyle="width:4em;" bodyStyle="text-align:center;" bodyClass="text-center"
-                sortable>
+            <Column
+                header="Tareas"
+                headerStyle="width:4em;"
+                bodyStyle="text-align:center;"
+                bodyClass="text-center"
+                sortable
+            >
                 <template #body="slotProps">
-                    <span v-if="
-                        slotProps.data.tareas_completadas !== undefined &&
-                        slotProps.data.tareas_total !== undefined
-                    ">
+                    <span
+                        v-if="
+                            slotProps.data.tareas_completadas !== undefined &&
+                            slotProps.data.tareas_total !== undefined
+                        "
+                    >
                         {{
                             "(" +
                             slotProps.data.tareas_completadas +
@@ -412,14 +536,35 @@ const flujoBody = (rowData) => {
             <Column header="" headerStyle="width:4em;">
                 <template #body="slotProps" class="text-center">
                     <div class="flex justify-center">
-                        <PrimaryButton class="pi pi-file-edit m-2" :href="route('minutas.edit', slotProps.data.id)">
-                            <span class="p-1" :style="{ fontSize: '10px' }">Editar</span>
+                        <div v-if="userPermissions.includes('minutas_editar')">
+                            <PrimaryButton
+                                class="pi pi-file-edit m-2"
+                                :href="route('minutas.edit', slotProps.data.id)"
+                            >
+                                <span class="p-1" :style="{ fontSize: '10px' }"
+                                    >Editar</span
+                                >
+                            </PrimaryButton>
+                        </div>
+
+                        <PrimaryButton
+                            class="pi pi-list m-2"
+                            :href="route('minutas.show', slotProps.data.id)"
+                        >
+                            <span class="p-1" :style="{ fontSize: '10px' }"
+                                >Tareas</span
+                            >
                         </PrimaryButton>
-                        <PrimaryButton class="pi pi-list m-2" :href="route('minutas.show', slotProps.data.id)">
-                            <span class="p-1" :style="{ fontSize: '10px' }">Tareas</span>
-                        </PrimaryButton>
-                        <PrimaryButton class="pi pi-trash m-2" @click.prevent="deleteMinuta(slotProps.data.id)">
-                        </PrimaryButton>
+
+                        <div
+                            v-if="userPermissions.includes('minutas_eliminar')"
+                        >
+                            <PrimaryButton
+                                class="pi pi-trash m-2"
+                                @click.prevent="deleteMinuta(slotProps.data.id)"
+                            >
+                            </PrimaryButton>
+                        </div>
                     </div>
                 </template>
             </Column>
