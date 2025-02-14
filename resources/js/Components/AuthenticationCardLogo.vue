6465674cb9a29532f1f4e2f9dd48e8b8cd797c33
jesuslim <limchuy7@gmail.com>
2025-02-14 11:07:31 -0600

    feat: logo change and background

--- resources/js/Components/AuthenticationCardLogo.vue
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
     <Link :href="'/'">
-    <!-- Logo -->
-    <!-- <img src="../../img/logos/Logotipo_AIONbco.png" alt="Logo" class="h-[32rem]" /> -->
-    <img src="../../img/logos/logoAionBusinessBco.png" alt="Logo" class="h-[12rem]" />
-
+        <!-- Logo -->
+        <!-- <img src="../../img/logos/Logotipo_AIONbco.png" alt="Logo" class="h-[32rem]" /> -->
+        <div class="flex justify-center bg-slate-100 opacity-20 rounded-"></div>
+        <img src="../../img/logos/AION_LOGO.png" alt="Logo" class="h-[12rem]" />
     </Link>
 </template>
