83ec8a11e6029c6029e29a6783775bf4c79fc92a
jesuslim <limchuy7@gmail.com>
2025-02-17 09:42:49 -0600

    refactor: change logo image, remove gradient

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-logo bg-gradient-to-b from-slate-100 to-slate-950"
+        class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-logo"
     >
         <div>
             <slot name="logo" />
--- resources/js/Components/AuthenticationCardLogo.vue
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -7,6 +7,10 @@ import { Link } from "@inertiajs/vue3";
         <!-- Logo -->
         <!-- <img src="../../img/logos/Logotipo_AIONbco.png" alt="Logo" class="h-[32rem]" /> -->
         <div class="flex justify-center bg-slate-100 opacity-20 rounded-"></div>
-        <img src="../../img/logos/AION_LOGO.png" alt="Logo" class="h-[12rem]" />
+        <img
+            src="../../img/logos/logo_aion_v2.png"
+            alt="Logo"
+            class="h-[12rem]"
+        />
     </Link>
 </template>
