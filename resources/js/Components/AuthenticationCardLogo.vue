671b87dbcd2935fd54b900dc36b9da411cae56c1
jesuslim <limchuy7@gmail.com>
2025-03-04 17:15:00 -0600

    feat: update logo

--- resources/js/Components/AuthenticationCardLogo.vue
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -8,7 +8,7 @@ import { Link } from "@inertiajs/vue3";
         <!-- <img src="../../img/logos/Logotipo_AIONbco.png" alt="Logo" class="h-[32rem]" /> -->
         <div class="flex justify-center bg-slate-100 opacity-20 rounded-"></div>
         <img
-            src="../../img/logos/logo_aion_v2.png"
+            src="../../img/logos/AION_LOGO_V2.png"
             alt="Logo"
             class="h-[12rem]"
         />
