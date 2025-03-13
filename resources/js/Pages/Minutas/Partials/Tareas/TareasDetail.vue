21dd9cc2eb44199ab3f4752dabdbdbcd4022361f
jesuslim <limchuy7@gmail.com>
2025-03-13 15:52:39 -0600

    feat: notas en la tarea aceptan url, solo en detalle

--- resources/js/Pages/Minutas/Partials/Tareas/TareasDetail.vue
+++ resources/js/Pages/Minutas/Partials/Tareas/TareasDetail.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed } from "vue";
 import { useForm } from "@inertiajs/vue3";
 import axios from "axios";
 import InputLabel from "@/Components/InputLabel.vue";
@@ -126,6 +126,18 @@ const getEvidencias = async () => {
         console.error(error);
     }
 };
+
+const convertUrlsToLinks = (text) => {
+    if (!text) return "";
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.replace(
+        urlRegex,
+        (url) =>
+            `<a href="${url}" target="_blank" class="text-blue-500 underline">${url}</a>`
+    );
+};
+
+const formattedNota = computed(() => convertUrlsToLinks(form.nota));
 </script>
 
 <template>
@@ -248,15 +260,11 @@ const getEvidencias = async () => {
                         </div>
                         <div class="mt-4">
                             <InputLabel for="nota" value="Notas: " />
-                            <Textarea
-                                class="mt-1 block w-full"
-                                v-model="form.nota"
-                                rows="5"
-                                cols="30"
-                                disabled
-                            />
+                            <p
+                                v-html="formattedNota"
+                                class="mt-1 block w-full border p-2 bg-gray-100 rounded"
+                            ></p>
                         </div>
-
                         <div class="flex items-center justify-end mt-4">
                             <PrimaryButton
                                 @click="emit('close')"
